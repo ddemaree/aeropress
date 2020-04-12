ea91352a18db44c7d16e5628f509b0d72c0be776
Glitch (hello-sqlite) <none>
2020-04-12 01:34:14 +0000

    ✂️🎩 Checkpoint ./pages/index.js:37758/162

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import Head from 'next/head'
+import { Container, Editor, Toolbar } from 'react-mobiledoc-editor'
 
 const HomePage = () => {
   return <>
     <Head>
       <title>David's project</title>
     </Head>
-    <div><h1>I'm on Next JS!!!!!!</h1></div>
+    <div>
+      <h1>I'm on Next JS!!!!!!</h1>
+      <Container>
+        <Editor />
+      </Container>
+    </div>
   </>
 }
 
