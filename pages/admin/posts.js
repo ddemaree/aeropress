ec1c6814c1c1cc88d60b351432336ca7620ea7ba
Glitch (crystal-mixed-gambler) <none>
2020-04-18 18:49:19 +0000

    👵📺 Checkpoint ./pages/admin/posts.js:37758/1885 ./pages/api/posts.js:37758/163

--- pages/admin/posts.js
+++ pages/admin/posts.js
@@ -1,19 +1,29 @@
 import { useState, useEffect } from 'react'
 
-const PostsIndex = ({ }) => {
+const PostsIndex = () => {
   const [posts, setPosts] = useState([])
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    fetch("/api/posts", {method: 'GET'})
+  // If 
+  const updatePosts = () => {
+    setLoading(true)
+    return fetch("/api/posts", {method: 'GET'})
       .then(response => response.json())
       .then(data => {
-      
-    })
-  })
+        setPosts(data.posts)
+        setLoading(false)
+      })
+  }
+  
+  useEffect(() => {
+    updatePosts()
+  }, [1])
   
   return <div>
-    
+    {loading && <div>Loading...</div>}
+    {!loading && posts.map(post => <div key={post.id} className="admin-post">
+      <div>{post.title}</div>
+    </div>)}
   </div>
 }
 
--- pages/api/posts.js
+++ pages/api/posts.js
@@ -8,8 +8,11 @@ export default async (req, res) => {
     const post = await Post.create({ title, meta: {hello: 'world'} })
     res.status(201).json({ message: `Created post with ID ${post.id} ${post.cuid}`, post: { id: post.id, cuid: post.cuid } })
   } else {
-    const { count, rows } = await Post.findAndCountAll({ limi})
+    
+    const { count, rows } = await Post.findAndCountAll({ limit: 50, offset: 0})
+    const posts = rows.map(row => ({ id: row.id, title: row.title }))
+    
     // Handle any other HTTP method
-    res.status(200).json({ name: 'Next.js posts/index' })
+    res.status(200).json({ name: 'Next.js posts/index', posts })
   }
 }
