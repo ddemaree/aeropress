ff69a0070b164a1047088b174101931ee7de772e
Glitch (crystal-mixed-gambler) <none>
2020-04-18 18:25:12 +0000

    🌠👕 Checkpoint ./pages/admin/posts.js:37758/356

---
+++ pages/admin/posts.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+
+const PostsIndex = ({ }) => {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    
+  })
+  
+  return <div>
+    
+  </div>
+}
+
+export default PostsIndex
