6826df578d9675f4eb2af70eec4067f2e8d48c24
Glitch (crystal-mixed-gambler) <none>
2020-04-20 01:39:26 +0000

    📈💷 Checkpoint ./pages/api/authorize.js:37758/1383 ./package.json:37758/1868

--- pages/api/authorize.js
+++ pages/api/authorize.js
@@ -1,9 +1,21 @@
 import { User } from '../../models'
+import bcrypt from 'bcrypt-nodejs'
+import jwt from 'jsonwebtoken'
+
+const THE_SECRET = process.env.SECRET_KEY || "hard work and healthy food"
 
 export default async (req, res) => {
   if (req.method === 'POST') {
-    res.status(200).json({ msg: "hello there" })
+    const { username, password } = req.body
+    const user = await User.findOne({where: { email: username }})
     
+    if(user && bcrypt.compareSync(password, user.password_digest)) {
+      const token = jwt.sign({ id: user.cuid }, THE_SECRET)
+
+      res.status(200).json({ msg: "Passwords match, send back token", token })
+    } else {
+      res.status(401).json({ msg: "Could not authenticate user" })
+    }
   }
   else {
     res.status(404).json({ msg: "Not sure what yr doing here" })
