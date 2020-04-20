beee5cc9eed86dc7efdc3e0e0998cd4eb599b7ee
Glitch (crystal-mixed-gambler) <none>
2020-04-20 00:07:22 +0000

    🏨💽 Checkpoint ./components/auth-provider.js:37758/873

--- components/auth-provider.js
+++ components/auth-provider.js
@@ -1,5 +1,17 @@
+import { createContext, useState, useReducer } from 'react';
+
 const AUTH_EXPIRES_AT = 'ap_auth_expires_at';
 const AUTH_USER = 'ap_auth_user';
+
+const actions = {
+  AUTH_START: 'START_AUTH',
+  AUTH_END: 'END_AUTH',
+  
+}
+
+const authReducer = (state, action) => {
+  return state;
+}
 
 const getDefaultState = () => {
   const DEFAULT_STATE = {
@@ -22,4 +34,26 @@ const getDefaultState = () => {
       }
     }
   }
+  
+  return {
+    ...DEFAULT_STATE,
+    ...storedState
+  }
+};
+
+export const AuthContext = createContext({
+  state: getDefaultState(),
+  dispatch: () => {}
+});
+
+export const AuthProvider = ({ children }) => {
+  const [ state, dispatch ] = useReducer(authReducer, getDefaultState())
+  const [ contextValue, setContextValue ] = useState({
+    state,
+    dispatch
+  })
+  
+  return <AuthContext.Provider value={contextValue}>
+    {children}
+  </AuthContext.Provider>
 }
