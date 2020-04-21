9912c4b0c3371e3791da6e058d5c19ba093723e3
Glitch (crystal-mixed-gambler) <none>
2020-04-21 00:38:33 +0000

    🍪🍇 Checkpoint ./pages/admin/posts/[postId].js:37758/156

--- pages/admin/posts/[postId].js
+++ pages/admin/posts/[postId].js
@@ -9,13 +9,13 @@ const ExpandoTextArea = ({ value, placeholder, className, onChange }) => {
 //   }, [])
   
   const handleChange = e => {
-    console.log(e.target)
+    console.log(e.target.value, e.target.offsetHeight)
     
     if(typeof onChange === 'function') onChange(e)
   }
   
   return <textarea 
-    className={c('bg-gray-100 text-gray-900 text-2xl py-3 px-3', className)}
+    className={c('bg-gray-100 text-gray-900 text-4xl py-3 px-3 w-full mx-auto text-center', className)}
     ref={textareaRef}
     placeholder={placeholder}
     style={{overflow: 'hidden', overflowWrap: 'break-word', resize: 'none'}}
@@ -24,7 +24,7 @@ const ExpandoTextArea = ({ value, placeholder, className, onChange }) => {
 
 
 const EditView = () => {
-  return <div>
+  return <div className="max-w-4xl mx-auto">
     <h1>Edit post</h1>
     <ExpandoTextArea placeholder="Add title" />
   </div>
