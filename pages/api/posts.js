2032e1157bcd10141a1e8c8f5b9b516949532ff5
Glitch (crystal-mixed-gambler) <none>
2020-04-21 00:02:35 +0000

    🌜📺 Checkpoint ./pages/api/posts.js:37758/83

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -3,17 +3,12 @@ import { omit } from 'lodash'
 
 export default async (req, res) => {
   if (req.method === 'POST') {
-    // Process a POST request
     const { title } = req.body
-
     const post = await Post.create({ title, meta: {hello: 'world'} })
     res.status(201).json({ message: `Created post with ID ${post.id} ${post.cuid}`, post: { id: post.id, cuid: post.cuid } })
   } else {
-    
     const { count, rows } = await Post.findAndCountAll({ limit: 50, offset: 0})
     const posts = rows.map(row => omit(row.toJSON(), ['id']))
-
-    // Handle any other HTTP method
     res.status(200).json({ count, posts })
   }
 }
