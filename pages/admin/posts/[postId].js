7d33a1d40241761f30516e2668e6ae79c3a3851d
Glitch (crystal-mixed-gambler) <none>
2020-04-21 01:37:30 +0000

    😇⛺️ Checkpoint ./pages/admin/posts/[postId].js:37758/910

--- pages/admin/posts/[postId].js
+++ pages/admin/posts/[postId].js
@@ -5,14 +5,17 @@ const ExpandoTextArea = ({ value, placeholder, className, onChange }) => {
   const textareaRef = useRef(null)
   const phantomRef = useRef(null)
   const [fieldValue, setFieldValue] = useState(value || '')
+  const [fieldHeight, setFieldHeight] = useState(122)
 
-//   const updateTextareaHeight = () => {
+  const updateTextareaHeight = () => {
     
-//   }
+  }
   
   useEffect(() => {
     console.log(phantomRef.current.offsetHeight)
-  })
+    
+    setFieldHeight(phantomRef.current.offsetHeight)
+  }, [fieldValue])
   
   const handleChange = e => {
     console.log(e.target.value)
@@ -28,12 +31,16 @@ const ExpandoTextArea = ({ value, placeholder, className, onChange }) => {
       className={c(defaultClasses, className)}
       ref={textareaRef}
       placeholder={placeholder}
-      style={{overflow: 'hidden', overflowWrap: 'break-word', resize: 'none'}}
+      style={{overflow: 'hidden', overflowWrap: 'break-word', resize: 'none', height: fieldHeight}}
       onChange={handleChange}
       value={fieldValue} />
-    <div
-      ref={phantomRef}
-      className={c(defaultClasses)}>{fieldValue}</div>
+    <div style={{height: 0, position: 'relative', overflow: 'hidden'}}>
+      <div
+        ref={phantomRef}
+        className={c(defaultClasses)}
+        style={{overflow: 'hidden', opacity: 0.3}}
+        aria-hidden={true}>{fieldValue}</div>
+    </div>
   </Fragment>
 }
 
@@ -42,6 +49,8 @@ const EditView = () => {
   return <div className="max-w-4xl mx-auto">
     <h1>Edit post</h1>
     <ExpandoTextArea placeholder="Add title" value="When the Pawn Hits the Conflicts He Thinks Like a King What He Knows Throws the Blows When He Goes to the Fight and He'll Win the Whole Thing 'fore He Enters the Ring" />
+    
+    <div>Rich text editor here!!!</div>
   </div>
 }
 
